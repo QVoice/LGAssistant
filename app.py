e7fec8c87fc11e4cb117fa65930dd539faf9b019
QVoice <a-qvoice-lge@lge.com>
2017-02-13 15:44:38 +0900

    sendFCM again & import requests

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ standard_library.install_aliases()
 import urllib.request, urllib.parse, urllib.error
 import json
 import os
+import requests
 
 from flask import Flask
 from flask import request
@@ -37,7 +38,7 @@ def webhook():
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
 
-    # sendFCM()
+    sendFCM()
 
     return r
 
