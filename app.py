cacbf110f981f3ee53db7285932bf5d76df6ac78
dextto <zd.gary@gmail.com>
2017-02-13 16:36:46 +0900

    use pyfcm

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ from flask import Flask
 from flask import request
 from flask import make_response
 
+from pyfcm import FCMNotification
+
 # Flask app should start in global layout
 app = Flask(__name__)
 
@@ -44,6 +46,13 @@ def webhook():
 
 
 def sendFCM():
+    push_service = FCMNotification(api_key="AAAAPll10tw:APA91bFgF4IU5k7V4-YBEphx9k7y7z0pqyhGcnN3Qbk8Wjuglftq8MzBa_ST75j4HSNh0YaonJov0BtTtq_85i8ao0Fm92JlDit96xLY5UJiC_OVwfFNvpYJnFU5FRYA7M8O20i2ahEi")
+    to="fqfSBcsFu7A:APA91bH8Ob2tgbirkUBAjPSwF4kZJGto5fFMousw8hRWo6AkutpIkFLauecEzSNUDtJoug92RJ7D1bVcq32rX5sWiIIKvNQwgZHswU2xIrYlsAS8BHrBQSvPuJVaV0117tqRwBn0dq1z"
+    message = "Hope you're having fun this weekend, don't forget to check today's news"
+    result = push_service.notify_single_device(registration_id=registration_id)
+
+'''
+def sendFCM():
     url = 'https://fcm.googleapis.com/fcm/send'
     body = {
         "data":{
@@ -62,7 +71,7 @@ def sendFCM():
     r = requests.post(url, data=json.dumps(body, indent=4), headers=headers)
     print(r)
     return r
-
+'''
 
 def processRequest(req):
     if req.get("result").get("action") != "yahooWeatherForecast":
