fb5bae69432da16aaa9e88426940e40c40394f2f
QVoice <a-qvoice-lge@lge.com>
2017-02-14 14:24:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,6 @@ from flask import Flask
 from flask import request
 from flask import make_response
 
-from pyfcm import FCMNotification
-
 # Flask app should start in global layout
 app = Flask(__name__)
 
@@ -32,21 +30,6 @@ def sendFCM():
     print("sendFCM result:")
     print(r)
     return r
-
-def sendNoti():
-    push_service = FCMNotification(api_key="AAAAPll10tw:APA91bFgF4IU5k7V4-YBEphx9k7y7z0pqyhGcnN3Qbk8Wjuglftq8MzBa_ST75j4HSNh0YaonJov0BtTtq_85i8ao0Fm92JlDit96xLY5UJiC_OVwfFNvpYJnFU5FRYA7M8O20i2ahEi")
-    registration_id="fqfSBcsFu7A:APA91bH8Ob2tgbirkUBAjPSwF4kZJGto5fFMousw8hRWo6AkutpIkFLauecEzSNUDtJoug92RJ7D1bVcq32rX5sWiIIKvNQwgZHswU2xIrYlsAS8BHrBQSvPuJVaV0117tqRwBn0dq1z"
-    message = "Hope you're having fun this weekend, don't forget to check today's news"
-    result = push_service.notify_single_device(registration_id=registration_id, message_body=message, message_title="title")
-    print(result)
-    return None
-
-def test():
-    URL = 'http://www.tistory.com'
-    response = requests.get(URL)
-    print('test status: ')
-    print(response.status_code)
-    return None
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
